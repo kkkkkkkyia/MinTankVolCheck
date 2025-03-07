0dc87d628ce4dad78376ed5f1a7a497db492e03b
kkkkkkkyia <yuhui.wu@oneh2.com>
2025-03-07 09:12:52 -0800

    modify

--- MinTankVolCheck.h
+++ MinTankVolCheck.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "IRread.h" 
 
-#define MIN_TANK_VOLUME 49.7  // SAE J2601 minimum required volume
+#define MIN_TANK_VOLUME_A 49.7  // SAE J2601 CHSS minimum required volume
 
 // Function prototypes
 bool checkMinTankVolume(const CHSS &chss);
